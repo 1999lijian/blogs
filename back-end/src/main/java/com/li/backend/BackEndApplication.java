62abfc6ba6e67e256d7ef48d5817983acd1a4dda
1999lijian <1014135810@qq.com>
2024-09-23 13:40:15 +0800

    后端搭建

--- back-end/src/main/java/com/li/backend/BackEndApplication.java
+++ back-end/src/main/java/com/li/backend/BackEndApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//test
+//开始
 @SpringBootApplication
 public class BackEndApplication {
 
